5e08414d1c87e1b49af8a271e939b289ac26c58f
Nazarii Lavrenchuk <jackil182@gmail.com>
2019-01-12 14:10:54 +0200

    practiced operating with browser DOM, more functional programming

--- Ex5-8/index.js
+++ Ex5-8/index.js
@@ -623,3 +623,362 @@
 // console.log('value: ', myCar.value); // 2000
 // myCar.value = 4000;
 // console.log('value: ', myCar.value); // 4000
+
+
+//===========================================================
+
+
+// /*
+//   Есть список категорий с классом categories (на вкладке HTML).
+  
+//   Напишите код, который для каждого элемента li (первая вложенность) 
+//   в списке categories выведет в консоль:
+//   - Текст непосредственно в нём (название категории)
+//   - Количество всех вложенных в него элементов li
+  
+//   К примеру:
+//     Категория: Животные
+//     Количество вложенных li: 4
+// */
+
+// const list = document.querySelectorAll('.categories > li');
+
+// const result = list.forEach(elem => {
+//     let category = elem.firstChild.textContent.trim();
+//     let = numberOfItems = elem.querySelector('ul').children.length;
+//     console.log(`
+//         Категория: ${category}
+//         Количество вложенных li: ${numberOfItems}
+//         `);
+// })
+
+
+//===========================================================
+
+
+// /*
+//   Дан список с классом .list
+// 	- Найдите первого потомка списка и сделайте его текст красного цвета
+// 	- Найдите последнего потомка списка и сделайте его текст синего цвета
+// */
+
+// const list = document.querySelector('.list');
+// // console.log(list);
+
+// const firstItem = list.firstElementChild;
+// firstItem.style.color = 'red';
+
+// const lastItem = list.lastElementChild;
+// lastItem.style.color = 'blue';
+
+
+//===========================================================
+
+
+// /*
+//   Дан ul склассом .list и массив строк. 
+  
+//   Вставьте элементы этого массива в ul так, чтобы каждый элемент стоял в своем li.
+// */
+
+// const elements = ['HTML', 'CSS', 'JavaScript', 'React', 'Node'];
+// const list = document.querySelector('.list');
+// const item = document.createElement('li');
+
+// let i = 0;
+// const numberOfItems = elements.length;
+
+// do {
+//     const clone = item.cloneNode(true);
+//     clone.textContent = elements[i]
+//     list.append(clone);  
+//     i++;
+// } while (i < numberOfItems) 
+
+// console.log(list);
+
+
+//===========================================================
+
+
+// /*
+//   Напишите скрипт для создания галлереи изображений. 
+  
+//   - На вкладке HTML уже есть ul.gallery.
+//   - Используйте массив объектов для создания тегов img вложенных в li
+//   - Оформление по вкусу, можно и не делать, достаточно чтобы каждое изображение было 300px по ширине
+//   - Добавьте все элементы галлереи в ul.gallery
+// */
+
+// const galleryItems = [
+//     {
+//       url:
+//         "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//       alt: "White and Black Long Fur Cat"
+//     },
+//     {
+//       url:
+//         "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//       alt: "Orange and White Koi Fish Near Yellow Koi Fish"
+//     },
+//     {
+//       url:
+//         "https://images.pexels.com/photos/1216482/pexels-photo-1216482.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//       alt: "Two Brown Hen and One Red Rooster"
+//     },
+//     {
+//       url:
+//         "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//       alt: "Group of Horses Running"
+//     },
+//     {
+//       url:
+//         "https://images.pexels.com/photos/1316294/pexels-photo-1316294.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//       alt: "Macaw Birds"
+//     },
+//     {
+//       url:
+//         "https://images.pexels.com/photos/41178/africa-animal-big-carnivore-41178.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+//       alt: "2 Lion on Grass Field during Daytime"
+//     }
+//   ];
+  
+
+// const gallery = document.querySelector('.gallery');
+
+// const item = document.createElement('li');
+// item.style.listStyle = 'none';
+
+// const image = document.createElement('img');
+// image.style.width = '300px';
+
+// item.append(image);
+
+// let i = 0;
+// const numberOfImgs = galleryItems.length;
+
+// do {
+//     image.src = galleryItems[i]['url'];
+//     image.alt = galleryItems[i]['alt'];
+//     const clone = item.cloneNode(true);
+//     gallery.append(clone);
+//     i++;
+// } while (i < numberOfImgs)
+
+// console.log(gallery);
+
+
+//===========================================================
+
+
+// /*
+//   Есть список с классом .size-filter из произвольного количества чекбоксов, каждый из которых содержит размер одежды в фильтре.
+  
+//   Напишите функцию collectInputData(inputs), которая принимает 1 параметр inputs - массив тех инпутов у которых состояние checked.
+  
+//   Возвращает массив значений атрибута value.
+// */
+
+// const checkboxes = document.querySelectorAll('input');
+// // const checkboxes = document.querySelectorAll('.size-filter > li > label > input');
+// // console.log(checkboxes);
+
+// const inputsArray = [];
+// checkboxes.forEach(elem => 
+//     elem.hasAttribute('checked') && elem.getAttribute('type') === 'checkbox' 
+//         ? inputsArray.push(elem)  
+//         : inputsArray);
+// // console.log(inputsArray);
+
+// const collectInputData = (inputs) => {
+//     let result = [];
+//     inputs.forEach(x => result.push(x.getAttribute('value')));
+//     return result;
+// }
+
+// console.log(collectInputData(inputsArray));
+
+// // const collectInputData = (inputs) => {
+// //     let result = [];
+
+// //     checkboxes.forEach(elem => elem.hasAttribute('checked') 
+// //     ? result.push(elem.getAttribute('value'))  
+// //     : result);
+
+// //     console.log(result);    
+// // }
+
+
+//===========================================================
+
+
+// /*
+//   Создайте функцию createMovieCard(), которая создает и возвращает DOM-узел карточки кинофильма.
+//   Разметка с классами есть на вкладке HTML. Стили на вкладке CSS.
+//   Используйте createElement для создания узлов. Добавьте классы и атрибуты.
+// */
+
+// const movieList = [
+//     {
+//         image: {
+//             src: 'http://image.tmdb.org/t/p/w500/rPdtLWNsZmAtoZl9PK7S2wE3qiS.jpg',
+//             alt: 'movie image',
+//         },
+//         title: 'The Godfather',
+//         description: 'Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch, Vito Corleone barely survives an attempt on his life, his youngest son, Michael steps in to take care of the would-be killers, launching a campaign of bloody revenge.',
+//         date: 'Released: 1972-03-14',
+//         rating: 'Rating: 8.6',
+//     },
+
+//     {
+//         image: {
+//             src: 'https://images.wolfgangsvault.com/the-lord-of-the-rings-the-definitive-movie-posters/book/memorabilia/ZZZ14649-BK.jpg', 
+//             alt: 'movie image',
+//         },
+//         title: 'The Lord of the Rings',
+//         description: 'Based on the J.R.R. Tolkien\'s trilogy, lorem ipsum isit dolor adispicum bla bla amily. When organized crime family patriarch, Vito Corleone barely surviv and so on, because asdf nskdjn f;as kdn ajd this movie is very good. The director did a great job ',
+//         date: 'Released: 2001-09-24',
+//         rating: 'Rating: 8.8',
+//     },
+// ];
+
+// const moviesGrid = document.querySelector('.movies-grid');
+
+// const elements = createMovieList(movieList);
+// moviesGrid.append(...elements);
+
+
+// //this function creates a single movie card
+// function createMovieCard({image, title, description, date, rating}) {
+
+//     const movieCard = document.createElement('div');
+//     movieCard.classList.add('movie');
+
+//     const movieImg = document.createElement('img');
+//     movieImg.classList.add('movie__image');
+//     movieImg.setAttribute('src', image.src);
+//     movieImg.setAttribute('alt', image.alt);
+    
+//     const movieBody = document.createElement('div');
+//     movieBody.classList.add('movie__body');
+    
+//     const movieTitle = document.createElement('h2');
+//     movieTitle.classList.add('movie__title');
+//     movieTitle.textContent = title;
+    
+//     const movieDescription = document.createElement('p');
+//     movieDescription.classList.add('movie__description');
+//     movieDescription.textContent = description;
+    
+//     const movieDate = document.createElement('p');
+//     movieDate.classList.add('movie__date');
+//     movieDate.textContent = date;
+    
+//     const movieRating = document.createElement('p');
+//     movieRating.classList.add('movie__rating');
+//     movieRating.textContent = rating;
+    
+    
+//     movieBody.append(movieTitle, movieDescription, movieDate, movieRating);
+//     movieCard.append(movieImg, movieBody);
+
+//     return movieCard;
+// }
+
+// //this function creates multiple movie cards from array
+// function createMovieList(arr) {
+
+//     const elements = [];
+//     arr.forEach(elem => {
+//         const el = createMovieCard(elem);
+//         elements.push(el);
+//     });
+
+//     return elements;
+// };
+
+// console.log(elements);
+
+
+// // console.log(movieCard);
+// // console.log(movieImg);
+// // console.log(movieTitle);
+// // console.log(movieDescription);
+// // console.log(movieDate);
+// // console.log(movieDate);
+
+
+//===========================================================
+
+
+/*
+  В HTML-документе уже есть тег с id="root" (вкладка HTML)
+  
+  Создайте функцию createBoxes(num), которая принимает 1 параметр num - число.
+  
+  Функция создает столько div, сколько указано в num и возвращает их в одном
+  общем контейнере. После чего необходимо повесить результат работы функции
+  в div с id="#root"
+  
+  Каждый div:
+    - Имеет случайный rgb цвет фона
+    - Размеры самого первого div - 30px на 30px.
+    - Каждый следующий div после первого, должен быть шире и выше предыдущего
+      на 10px
+*/
+
+const root = document.getElementById('root');
+// console.log(root);
+
+let boxSize = { height: 30, width: 30, };
+const addedBoxHeight = 10;
+const addedBoxWidth = 10;
+const boxes = [];
+
+function createBoxes(num) {
+    const box = document.createElement('div');
+
+    let i = 1;
+    do {
+        const clonedBox = box.cloneNode(true);
+
+        clonedBox.classList.add(`box${i}`);
+
+        clonedBox.style.backgroundColor = getRandomColor();
+        clonedBox.style.height = boxSize.height + 'px';
+        clonedBox.style.width = boxSize.width + 'px';
+        clonedBox.style.marginBottom = '10px';
+
+        boxes.push(clonedBox);
+        
+        changeBoxSize(addedBoxHeight, addedBoxWidth);
+
+        i++;
+    }
+    while (i <= num);
+    
+    root.append(...boxes);
+}
+
+
+function getRandomColor() {
+    const colorChars = '0123456789abcdef';
+    let color = '#';
+
+    for (let i = 0; i < 6; i +=1) {
+        color += colorChars[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function changeBoxSize(height, width) {
+    boxSize.height += height;    
+    boxSize.width += width;
+
+    return boxSize;
+}
+
+createBoxes(3);
+// console.log(getRandomColor());
+
+
