767faade578ce784524dab6f03cec6ada43268ec
Nazarii Lavrenchuk <jackil182@gmail.com>
2019-01-27 21:01:22 +0200

    regex, checkbox filtering, Handlebars, template parsing

---
+++ Ex11-12/index.js
@@ -0,0 +1,141 @@
+// /*
+//   К pen уже подключен Handlebars.
+//   Используйте встроенные шаблоны и метод Handlebars.compile
+//   Создайте шаблон списка указаного во вкладке HTML.
+//   Отрендерите список в DOM по данным из массива listItems.
+// */
+
+// const products = [
+//     { name: 'Apples', count: 50 },
+//     { name: 'Grapes', count: 44 },
+//     { name: 'Cheese', count: 128 },
+//     { name: 'Milk', count: 293 },
+//   ];
+
+// const list = document.querySelector('.products');
+
+// const source = document.querySelector("#list-template").innerHTML.trim();
+// const template = Handlebars.compile(source);
+// const markup = products.reduce((acc, item) => acc + template(item), '');
+
+// list.innerHTML = markup;
+// console.log(list);
+
+
+//===============================================================
+
+
+// /*
+//   Используйте встроенные шаблоны и метод Handlebars.compile
+  
+//   Создайте шаблон поста указаного во вкладке HTML. Отрендерите список постов в DOM по данным из массива posts.
+//   Если в объекте поле isFav=true, в посте должна быть разметка иконки избранного поста, в противном случае разметки иконки быть не должно.
+//   Используйте эту иконку для фона: https://image.flaticon.com/icons/svg/290/290413.svg
+// */
+
+// const posts = [
+//     {
+//       title: "Phasellus volutpat metus",
+//       text:
+//         "Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Ut leo.",
+//       isFav: true
+//     },
+//     {
+//       title: "Nulla consequat massa",
+//       text:
+//         "Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.",
+//       isFav: false
+//     },
+//     {
+//       title: "In enim justo",
+//       text:
+//         "Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Suspendisse eu ligula. Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus.",
+//       isFav: true
+//     },
+//     {
+//       title: "Vestibulum ante ipsum",
+//       text:
+//         "Vestibulum suscipit nulla quis orci. Praesent venenatis metus at tortor pulvinar varius. Nulla sit amet est.",
+//       isFav: false
+//     }
+//   ];
+  
+//   const list = document.querySelector('.posts');
+
+//   const source = document.querySelector('#posts-list').innerHTML.trim();
+//   const template = Handlebars.compile(source);
+//   const markup = posts.reduce((acc, item) => acc + template(item), '');
+
+//   list.innerHTML = markup;
+//   console.log(list);
+  
+
+//========================================================
+
+
+/* 
+  Напишите функцию validate которая проверяет поля формы firstname и lastname и возвращает результат в виде обьекта со свойствами 'first name' и 'last name'.
+  Кроме того, формат объекта: в свойства записывается буль-флаг уведомляющий о статусе прохождения валидации для каждого поля.
+  {
+    'first name': true или false,
+    'last name': true или false,
+  }
+  
+  Критерии валидации:
+  1)Имя. Допускается не более 2-х слов, разделенных пробелами или дефисом. Слова должны состоять только из букв.
+  2)Фамилия. Допускается не более 2-х слов, разделенных пробелами или дефисом. Слова должны состоять только из букв.
+  
+  При клике на кнопку submit должна происходить проверка. Результат проверки, объект, выводить в консоль.
+*/
+
+const form = document.querySelector('.js-form');
+const results = document.querySelector('.results');
+const item = document.createElement('li');
+
+const firstname = document.getElementById("first_name");
+const lastname = document.getElementById("last_name");
+const submitBtn = document.getElementById("submit-btn");
+
+const regEx = /^[a-z]{2,10}$|^[a-z]{2,10}-?\s?[a-z]{2,10}$/gi;
+
+submitBtn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+
+    results.innerHTML = '';
+    validate(firstname.value, lastname.value);
+    
+    form.reset();
+    });
+
+function validate(first, last) {
+    const firstNameLi = item.cloneNode(true);
+    const lastNameLi = item.cloneNode(true);
+
+    if (regEx.test(first)) {
+        firstNameLi.classList.add('success');
+        firstNameLi.classList.remove('error');
+        firstNameLi.innerHTML = `Your name **${first}** is valid`;
+    } else {
+        firstNameLi.classList.add('error');
+        firstNameLi.classList.remove('success');
+        firstNameLi.innerHTML = `Your name **${first}** is not valid`;
+    }
+    results.append(firstNameLi);
+
+    if (regEx.test(last)) {
+        lastNameLi.classList.add('success');
+        lastNameLi.classList.remove('error');
+        lastNameLi.innerHTML = `Your last name **${last}** is valid`;
+    } else {
+        lastNameLi.classList.add('error');
+        lastNameLi.classList.remove('success');
+        lastNameLi.innerHTML = `Your last name **${last}** is not valid`;
+    }
+    results.append(lastNameLi);
+
+    console.log({
+        'first name': regEx.test(first),
+        'last name':  regEx.test(first),
+    });
+}
+
---
+++ Homework-11/index.js
@@ -0,0 +1,310 @@
+/*
+  Реализуйте форму фильтра товаров в каталоге и список отфильтрованных товаров.
+  Используйте шаблонизацию для создания карточек товаров.
+  
+  Есть массив объектов (дальше в задании), каждый из которых описывает ноутбук с определенными характеристиками.
+  
+  Поля объекта по которым необходимо производить фильтрацию: size, color, release_date.
+  Поля объекта для отображения в карточке: name, img, descr, color, price, release_date.
+    
+  Изначально есть форма с 3-мя секциями, состоящими из заголовка и группы чекбоксов (разметка дальше в задании). После того как пользователь выбрал какие либо чекбоксы и нажал кнопку Filter, необходимо собрать значения чекбоксов по группам. 
+  
+  🔔 Подсказка: составьте объект формата
+      const filter = { size: [], color: [], release_date: [] }
+    
+  После чего выберите из массива только те объекты, которые подходят 
+  под выбраные пользователем критерии и отрендерите список карточек товаров.
+  
+  🔔 Каждый раз когда пользователь фильтрует товары, список карточек товаров очищается, после чего в нем рендерятся новые карточки товаров, соответствующих текущим критериям фильтра.
+*/
+
+
+const laptops = [
+    {
+      size: 13,
+      color: 'white',
+      price: 28000,
+      release_date: 2015,
+      name: 'Macbook Air White 13"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 13,
+      color: 'gray',
+      price: 32000,
+      release_date: 2016,
+      name: 'Macbook Air Gray 13"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 13,
+      color: 'black',
+      price: 35000,
+      release_date: 2017,
+      name: 'Macbook Air Black 13"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 15,
+      color: 'white',
+      price: 45000,
+      release_date: 2015,
+      name: 'Macbook Air White 15"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 15,
+      color: 'gray',
+      price: 55000,
+      release_date: 2016,
+      name: 'Macbook Pro Gray 15"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 15,
+      color: 'black',
+      price: 45000,
+      release_date: 2017,
+      name: 'Macbook Pro Black 15"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 17,
+      color: 'white',
+      price: 65000,
+      release_date: 2015,
+      name: 'Macbook Air White 17"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 17,
+      color: 'gray',
+      price: 75000,
+      release_date: 2016,
+      name: 'Macbook Pro Gray 17"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+    {
+      size: 17,
+      color: 'black',
+      price: 80000,
+      release_date: 2017,
+      name: 'Macbook Pro Black 17"',
+      img: 'http://demo.posthemes.com/pos_zadademo/images/placeholder.png',
+      descr:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, beatae.',
+    },
+  ];
+
+
+const form = document.querySelector('.js-form');
+const result = document.querySelector('.result');
+const filteredList = document.querySelector('#filtered-list');
+
+const btnSubmit = form.querySelector('.js-submit');
+const btnReset = form.querySelector('.js-reset');
+
+const inputs = document.querySelectorAll('input');
+const inputsArr = Array.from(inputs);
+
+let isActive = false;
+
+let filter = { 
+    size: [], 
+    color: [], 
+    release_date: [], 
+}
+
+btnSubmit.addEventListener('click', (evt) => {
+    evt.preventDefault();
+
+    if (isActive) return;
+    isActive = true;
+
+    const checkedInputs = inputsArr.filter(x => x.checked);
+    checkedInputs.forEach(input => {
+        if(input.name === 'size') {
+            filter.size.push(input.defaultValue)
+        }
+
+        if(input.name === 'color') {
+            filter.color.push(input.defaultValue)
+        }
+
+        if(input.name === 'release_date') {
+            filter.release_date.push(input.defaultValue)
+        }
+    });
+
+    console.log(filter);
+
+    filterLaptops(filter);
+
+    renderCards(checkedInputs);
+});
+
+
+btnReset.addEventListener('click', () => {
+    filter = { 
+        size: [], 
+        color: [], 
+        release_date: [], 
+    };
+
+    isActive = false;
+
+    result.innerHTML = '';
+})
+
+
+function renderCards(checkedInputs) {
+    const source = document.querySelector('#filtered-list').innerHTML.trim();    
+    const template = Handlebars.compile(source);
+    let qqq = filterLaptops(filter); 
+    const markup  = qqq.reduce((acc, item) => acc + template(item), '');
+
+    result.insertAdjacentHTML('afterbegin', markup);
+}
+
+function filterLaptops(filter) {
+
+    // console.log(filter.size);
+    
+    let zzz = [];
+    laptops.forEach(item => {
+        // console.log(item);
+
+        if (filter.size.length === 0) {
+            filter.color.forEach(x => {                        
+                if(item.color == x) {
+
+                    filter.release_date.forEach(x => {                        
+                        if(item.release_date == x) {
+                            zzz.push(item);
+                        }
+                    })
+
+                    // zzz.push(item);
+                }
+            })
+        }
+
+
+        if (filter.color.length === 0) {
+            filter.size.forEach(x => {
+                if(item.size == x) {
+                    filter.release_date.forEach(x => {              
+                    if(item.release_date == x) {
+                        zzz.push(item);
+                        }
+                    })
+                }
+            })
+        }
+
+
+        if (filter.release_date.length === 0) {
+            filter.size.forEach(x => {
+                if(item.size == x) {
+                    filter.color.forEach(x => {              
+                    if(item.color == x) {
+                        zzz.push(item);
+                        }
+                    })
+                }
+            })
+        }
+
+
+
+        if (filter.release_date.length === 0 && filter.color.length === 0 && filter.size.length === 0) {
+            zzz.push(item)
+        }
+
+
+        if (filter.release_date.length === 0 && filter.color.length === 0) {
+            filter.size.forEach(x => {
+                if(item.size == x) {
+                    zzz.push(item);
+                }
+            })
+        }
+
+
+        if (filter.size.length === 0 && filter.color.length === 0) {
+            filter.release_date.forEach(x => {
+                if(item.release_date == x) {
+                    zzz.push(item);
+                }
+            })
+        }
+
+
+
+        if (filter.size.length === 0 && filter.release_date.length === 0) {
+            filter.color.forEach(x => {
+                if(item.color == x) {
+                    zzz.push(item);
+                }
+            })
+        }
+
+        
+        filter.size.forEach(x => {
+            if(item.size == x) {
+
+                filter.color.forEach(x => {                        
+                    if(item.color == x) {
+
+                        filter.release_date.forEach(x => {                        
+                            if(item.release_date == x) {
+                                zzz.push(item);
+                            }
+                        })
+
+                        // zzz.push(item);
+                    }
+                })
+
+                // zzz.push(item);
+            }
+        })
+
+        // filter.color.forEach(x => {                        
+        //     if(item.color == x) {
+        //         zzz.push(item);
+        //     }
+        // })
+
+        // filter.release_date.forEach(x => {                        
+        //     if(item.release_date == x) {
+        //         zzz.push(item);
+        //     }
+        // })
+        
+    })
+    // console.log(zzz);
+
+    const unique = [...new Set(zzz)];
+
+    console.log(unique);
+    
+    
+    return unique;
+}
